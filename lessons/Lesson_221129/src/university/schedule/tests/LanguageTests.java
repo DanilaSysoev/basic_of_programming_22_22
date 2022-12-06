cd0c41f916582060bb6839fda0b32406737a44c9
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2022-12-06 11:11:06 +0300

    add @Before

--- lessons/Lesson_221129/src/university/schedule/tests/LanguageTests.java
+++ lessons/Lesson_221129/src/university/schedule/tests/LanguageTests.java
@@ -1,12 +1,24 @@
 package university.schedule.tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import university.schedule.core.Language;
 import university.schedule.core.LanguageCollection;
 
 public class LanguageTests extends Assert {
+    @Before
+    public void setup() {
+        LanguageCollection.clear();
+    }
+
     // nameMethod_situation_expectedResult
+    @Test
+    public void add_addIntoEmptyCollection_sizeEqualsOne() {
+        assertEquals(0, LanguageCollection.size());
+        LanguageCollection.add(new Language("english"));
+        assertEquals(1, LanguageCollection.size());
+    }
     @Test
     public void add_addIntoCollection_sizeIncrease() {
         int oldSize = LanguageCollection.size();
