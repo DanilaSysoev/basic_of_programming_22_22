982f1957797a6fdf6ba047478a72b1e262306432
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2022-12-06 11:58:56 +0300

    add_addTwoSameLanguages_sizeEqualsOne Ok

--- lessons/Lesson_221129/src/university/schedule/core/Language.java
+++ lessons/Lesson_221129/src/university/schedule/core/Language.java
@@ -1,13 +1,31 @@
 package university.schedule.core;
 
+import java.util.Objects;
+
 public class Language {
-    private String name;
+    private final String name;
 
     public Language(String name) {
         this.name = name;
     }
 
     public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Language language = (Language) o;
+        return Objects.equals(name, language.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+    @Override
+    public String toString() {
         return name;
     }
 }
